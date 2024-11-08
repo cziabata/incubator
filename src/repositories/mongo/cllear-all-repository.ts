8e0fb5f6c343df3cb9fff85e9381026628d30c87
Viacheslav Yemets <viacheslav.emets@gmail.com>
2024-11-08 22:59:00 +0200

    fixed db cleaning

--- src/repositories/mongo/cllear-all-repository.ts
+++ src/repositories/mongo/cllear-all-repository.ts
@@ -1,10 +1,11 @@
-import { blogsCollection, postsCollection } from "../../db/mongoDb";
+import { blogsCollection, postsCollection, usersCollection } from "../../db/mongoDb";
 
 export const clearAllRepository = {
   async deleteAll() {
     try {
       await blogsCollection.deleteMany({});
       await postsCollection.deleteMany({});
+      await usersCollection.deleteMany({});
       console.log("Both collections have been cleared.");
     } catch (e) {
       console.log("Error while clearing collections:", e);
