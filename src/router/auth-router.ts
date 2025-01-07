8894e35dddb133e5f72892889972551e789b8f92
Viacheslav Yemets <viacheslav.emets@gmail.com>
2025-01-07 21:54:48 +0200

    attempts guard for auth apis

--- src/router/auth-router.ts
+++ src/router/auth-router.ts
@@ -19,8 +19,8 @@ export const authRouter = Router();
 
 authRouter.post("/login", apiAttemptsGuard, ...loginUserValidators, loginController);
 authRouter.get("/me", accessTokenGuard, authMeController);
-authRouter.post("/registration", ...createUserValidators, registrationController);
-authRouter.post("/registration-confirmation", ...checkConfirmCodeValidators, confirmRegistrationController);
-authRouter.post("/registration-email-resending", ...resendEmailValidators, emailResendingController);
+authRouter.post("/registration", apiAttemptsGuard, ...createUserValidators, registrationController);
+authRouter.post("/registration-confirmation", apiAttemptsGuard, ...checkConfirmCodeValidators, confirmRegistrationController);
+authRouter.post("/registration-email-resending", apiAttemptsGuard, ...resendEmailValidators, emailResendingController);
 authRouter.post("/refresh-token", refreshTokenGuard, refreshTokenController);
 authRouter.post("/logout", refreshTokenGuard, logoutController);
